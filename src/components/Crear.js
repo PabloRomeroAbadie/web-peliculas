e0974c8ca240a80f10ab0e4b719e0b2b34fe747f
PabloRomeroAbadie <pabloabadie1995@gmail.com>
2023-05-22 10:57:31 -0300

    se realizo la funcion para conseguir los datos del form, crear un objeto y guardarlo

--- src/components/Crear.js
+++ src/components/Crear.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Crear = () => {
+    
+    const [peliState, setPeliState] = useState({
+        titulo: "",
+        descripcion: ""
+    });
+
+    //desestrucutrar el objeto
+    const {titulo, descripcion} = peliState;
+
+    const conseguirDatosForm = (e) => {
+        e.preventDefault();
+
+
+        //conseguir datos del formulario
+        let target = e.target;
+        let titulo = target.titulo.value;
+        let descripcion = target.descripcion.value;
+
+        //crear objeto de la pelicula a guardar
+        let peli = {
+            id: new Date().getTime(),
+            titulo, 
+            descripcion
+        }
+
+        setPeliState(peli);
+    }
+
     return (
         <div className="add">
-            <h3 className="title">Añadir Pelicula</h3>
-            <form>
-                <input type="text" placeholder="Titulo" />
-                <textarea placeholder="Descripcion"></textarea>
+            <h3 className="title">Añadir Pelicula:</h3>
+            
+            <strong>
+                {(titulo && descripcion) && "Has creado la pelicula: "+titulo}
+            </strong>
+
+            <form onSubmit={conseguirDatosForm}>
+                <input type="text" id='titulo' name="titulo" placeholder="Titulo" />
+                <textarea id='descripcion' name='descripcion' placeholder="Descripcion"></textarea>
                 <input type="submit" />
             </form>
         </div>
